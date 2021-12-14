07e465391211849d936d229d38d495ed64dcb117
Zarrar Jawed Ahmad Palekar <zarrar.palekar@syndicationpro.com>
2021-12-14 09:53:34 +0530

    Adding in Sanity Studio

---
+++ studio/schemas/testFile.js
@@ -0,0 +1,17 @@
+export default {
+  name: "test",
+  title: "Test",
+  type: "document",
+  fields: [
+    {
+      name: "testTitle",
+      title: "Test Title",
+      type: "string",
+    },
+    {
+      name: "testDescription",
+      title: "Test Description",
+      type: "text",
+    },
+  ],
+};
